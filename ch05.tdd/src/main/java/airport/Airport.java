e140a228f026d2bcf216bc0a8aeeb1c4302c254d
hamin <ggamini7@gmail.com>
2022-12-06 15:38:50 +0900

    add comments

--- ch05.tdd/src/main/java/airport/Airport.java
+++ ch05.tdd/src/main/java/airport/Airport.java
@@ -15,12 +15,12 @@ public class Airport {
         economyFlight.addPassenger(mike);
 
         System.out.println("Business flight passengers list:");
-        for (Passenger passenger : businessFlight.getPassengersList()) {
+        for (Passenger passenger : businessFlight.getPassengers()) {
             System.out.println(passenger.getName());
         }
 
         System.out.println("Economy flight passengers list:");
-        for (Passenger passenger : economyFlight.getPassengersList()) {
+        for (Passenger passenger : economyFlight.getPassengers()) {
             System.out.println(passenger.getName());
         }
     }
